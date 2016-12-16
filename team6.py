297f62e16d335b439ecc1cfc4ecba5b66d6bbce4
wmccotter <will.mccotter@vtsd.com>
2016-12-16 08:35:49 -0500

    final

--- team6.py
+++ team6.py
@@ -6,29 +6,28 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Example Text' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Kparisi13' # Only 10 chars displayed.
+strategy_name = 'Win'
+strategy_description = 'Only betray if we got screwed over last time'
     
+  
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    if len(my_history)==0:
+        return 'c'
+    else:
+        recent_round_them = their_history[-1]
+        recent_round_me = my_history[-1]
+        
+        for round in range(len(my_history)-1):
+            prior_round_them = their_history[round]
+            prior_round_me = my_history[round]
+            if (prior_round_me == recent_round_me) and \
+                    (prior_round_them == recent_round_them):
+                return their_history[round]
+            if my_history[-1]=='c' and their_history[-1]=='b':
+                return 'b' 
+        else:
+            return 'c'
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
